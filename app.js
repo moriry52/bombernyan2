fc89163e51cec7c18481bcf449feea6f21aaa5b4
moriry52 <moriry52@gmail.com>
2020-05-08 11:26:43 +0900

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(port);
 console.log("port", port)
 
 function handler(req, res) {
-	fs.readFile(__dirname + '/bomber.html',
+	fs.readFile(__dirname + '/index.html',
 		function (err, data) {
 			if (err) {
 				res.writeHead(500);
